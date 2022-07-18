6656036b8f93f3214a3bc0a72dc7e180d6bf49eb
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-07-18 13:08:11 +0000

    chore(release): v0.5.63

--- opentelekomcloud/jsii/jsii.go
+++ opentelekomcloud/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	cdktf      "github.com/hashicorp/terraform-cdk-go/cdktf/jsii"
 )
 
-//go:embed cdktf-provider-opentelekomcloud-0.5.62.tgz
+//go:embed cdktf-provider-opentelekomcloud-0.5.63.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@cdktf/provider-opentelekomcloud", "0.5.62", tarball)
+	_jsii_.Load("@cdktf/provider-opentelekomcloud", "0.5.63", tarball)
 }
